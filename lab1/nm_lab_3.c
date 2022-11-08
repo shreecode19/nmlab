17ca75ac1e9cfe7b00e3bbe489f3f59c101b2447
shreecode19 <shresthashreejan20@gmail.com>
2022-11-08 19:58:02 +0545

    ques3

--- lab1/nm_lab_3.c
+++ lab1/nm_lab_3.c
@@ -1,32 +1,57 @@
- //Write program to implement Horner’s method.
+/*Write a program to solve non-linear equation using Secant method. Your program should read
+an initial guess from keyboard and display the followings if the solution is obtained:
+a. Estimated root of the equation
+b. Functional value at calculated root
+c. Required number of iterations
+*/
 
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+
+/* Defining equation to be solved.
+   Change this equation to solve another problem. */
+#define    f(x)    x*x*x - 2*x - 5
 
 int main()
 {
-     float a[100],sum=0,x;
-     int n,i;
+	 float x0, x1, x2, f0, f1, f2, e, error;
+	 int step = 1;
 
-     printf("\nEnter degree of the polynomial X :: ");
-     scanf("%d",&n);
-     printf("\nEnter coefficient's of the polynomial X :: \n");
-     for(i=n;i>=0;i--)
-     {
-            printf("\nEnter Coefficient of [ X^%d ] :: ",i);
-            scanf("%f",&a[i]);
-     }
+	 /* Inputs */
+	 printf("\nEnter initial guesses:\n");
+	 scanf("%f%f", &x0, &x1);
+	 printf("Enter tolerable error:\n");
+	 scanf("%f", &e);
 
-     printf("\nEnter the value of X :: ");
-     scanf("%f",&x);
-
-     for(i=n;i>0;i--)
-     {
-        sum=(sum+a[i])*x;
-     }
-
-     sum=sum+a[0];
-
-     printf("\nValue of the polynomial is = [ %f ]\n",sum);
-
-     return 0;
+	 /* Implementing Secant Method */
+	 printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\t\tError\n");
+	 do
+	 {
+		  f0 = f(x0);
+		  f1 = f(x1);
+		  if(f0 == f1)
+		  {
+			   printf("Mathematical Error.");
+			   exit(0);
+		  }
+		  
+		  x2 = x1 - (x1 - x0) * f1/(f1-f0);
+		  f2 = f(x2);
+		  error = (x1 - x0)/x1;
+		  
+		  printf("%d\t\t%f\t%f\t%f\t%f\t%f\n",step,x0,x1,x2, f2, error);
+		  
+		  x0 = x1;
+		  f0 = f1;
+		  x1 = x2;
+		  f1 = f2;
+		  
+		  step = step + 1;
+		  
+	 }while(fabs(error)>e);
+	
+	 printf("\nRoot is: %f", x2);
+	 return 0;
 }
